b226bd5ddbb16af695f1a7d23d267bd17e2dae57
tniclas <ntauson@gmail.com>
2019-11-19 12:13:51 +0100

    SimulationMap tillagd som intreface, möjlighet att skifta mellan 4/6 noder i controller

--- src/entities/EntityNode.java
+++ src/entities/EntityNode.java
@@ -179,25 +179,25 @@ public class EntityNode extends Entity {
 			roads = roadsWest;
 			roads.add(road);
 			for (int i = 0; i < roads.size(); i++)
-				roads.get(i).setPosition(this, (int)getXPosition(), (int)getYPosition() + i * 24 + 24);
+				roads.get(i).setPosition(this, (int)getXPosition(), (int)getYPosition() + i * 40 + 12);
 			break;
 		case EAST:
 			roads = roadsEast;
 			roads.add(road);
 			for (int i = 0; i < roads.size(); i++)
-				roads.get(i).setPosition(this, (int)getXPosition() + getWidth(), (int)getYPosition() + i * 24 + 24);
+				roads.get(i).setPosition(this, (int)getXPosition() + getWidth(), (int)getYPosition() + i * 40 + 12);
 			break;
 		case NORTH:
 			roads = roadsNorth;
 			roads.add(road);
 			for (int i = 0; i < roads.size(); i++)
-				roads.get(i).setPosition(this, (int)getXPosition() + i * 48 + 24, (int)getYPosition());
+				roads.get(i).setPosition(this, (int)getXPosition() + i * 40 + 12, (int)getYPosition());
 			break;
 		case SOUTH:
 			roads = roadsSouth;
 			roads.add(road);
 			for (int i = 0; i < roads.size(); i++)
-				roads.get(i).setPosition(this, (int)getXPosition() + i * 48 + 24, (int)getYPosition() + getHeight());
+				roads.get(i).setPosition(this, (int)getXPosition() + i * 40 + 12, (int)getYPosition() + getHeight());
 			break;
 		}
 	}
