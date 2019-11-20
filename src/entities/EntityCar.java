6982dd3067d40a7206c977e3c73e9544e57eb6bd
Mattias <mattias.kallstrom@live.se>
2019-11-20 12:09:19 +0100

    Smal update with car graphics

--- src/entities/EntityCar.java
+++ src/entities/EntityCar.java
@@ -2,12 +2,14 @@ package entities;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Polygon;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
 public class EntityCar extends EntityVehicle{
 
 	private boolean smartVehicle = false;
+	private Polygon visionArea;
 	
 	
 	public EntityCar(EntityRoad road,PropertyChangeListener listener) {
@@ -19,6 +21,14 @@ public class EntityCar extends EntityVehicle{
 		super.castPropertyChange("CAR CREATED");
 	}
 	
+	public Polygon getVisionArea() {
+		if (visionArea == null) {
+			visionArea = new Polygon();
+			
+		}
+		return visionArea;		
+	}
+	
 	@Override
 	public void draw(Graphics g) {
 		super.draw(g);
@@ -27,6 +37,12 @@ public class EntityCar extends EntityVehicle{
 				g.setColor(Color.BLUE);
 			}
 			g.fillOval((int) getXPosition() - 8, (int) getYPosition() - 8, 16, 16);
+			
+			int alpha = 127; // 50% transparent
+			Color hitBoxYellow = new Color(235, 229, 52, alpha);
+			g.setColor(hitBoxYellow);
+			
+			//g.drawPolygon(visionArea);
 	}
 	
 }
